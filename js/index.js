154b34d2f8ed4d3a21f3664eaa86931c6398d8d8
SPTamim <white.hat.tamim@gmail.com>
2021-10-03 16:38:56 +0600

    TamimAhmed

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Type JS
+var typed = new Typed('.textType', {
+    strings: ['A Web Developer'],
+    smartBackspace: true,
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 100
+});
+
+//AOS
+AOS.init({
+	 offset: 150,
+	 duration: 1000
+});
